461e5fc1d4f5941d2e87b4e5842316e657bff512
skkim <skkim@sugarhill.co.kr>
2020-01-09 11:48:35 +0900

    환경구축 완료

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// router 설정
+var indexRouter = require('./routes/index');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// view 경로 설정
+app.set('views', __dirname + '/views');
+
+// 화면 engine을 ejs로 설정
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// 기본 path를 /public으로 설정(css, javascript 등의 파일 사용을 위해)
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', indexRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
